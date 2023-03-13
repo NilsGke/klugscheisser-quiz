3f6ffa476ed30b70782d5d2695541fa51b03e3dc
Nils Goeke <43449297+NilsGke@users.noreply.github.com>
2023-03-13 12:09:41 +0000

    setup firebase (store and auth for now)

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDuHvssabAB5X4fBMcXElXNZ2fb2bkGpiE",
+    authDomain: "quiz-game-952ec.firebaseapp.com",
+    projectId: "quiz-game-952ec",
+    storageBucket: "quiz-game-952ec.appspot.com",
+    messagingSenderId: "457268320506",
+    appId: "1:457268320506:web:64b07a3ec90723b28d5143",
+};
+
+export const app = initializeApp(firebaseConfig);
+
