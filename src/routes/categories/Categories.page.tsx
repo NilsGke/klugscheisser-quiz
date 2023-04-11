9f4c9a77cb17d4c8597a0c0af52d38f6a35c28f7
NilsGke <n.j.goeke@gmail.com>
2023-04-11 22:33:03 +0200

    now uses CategoryBrowser component

--- src/routes/categories/Categories.page.tsx
+++ src/routes/categories/Categories.page.tsx
@@ -1,136 +1,14 @@
-import { FC, useCallback, useEffect, useRef, useState } from "react";
 import HomeButton from "../../components/HomeButton";
 import "./Categories.page.scss";
-import DotSpinner from "../../components/DotSpinner";
 import { Category } from "../../types/categoryTypes";
+import CategoryBrowser from "../../components/CategoryBrowser";
 
-export enum CategoriesPagePurpose {
-    VIEW_CATEGORIES,
-    SELECT_CATEGORIES,
-}
-
-type props = {
-    defaultPurpose: CategoriesPagePurpose;
-};
-
-// TEMP this is only temporary and will be replaced with proper search feature that searches on users device (indexeddb)
-
-const Categories: FC<props> = ({ defaultPurpose }) => {
-    const [purpose, setPurpose] =
-        useState<CategoriesPagePurpose>(defaultPurpose);
-    // #region search
-    const [lastDoc, setLastDoc] = useState<undefined>(undefined);
-
-    const [searchTerm, setSearchTerm] = useState("");
-
-    const search = useCallback(async () => {
-        // setGotAllCategories(false);
-        // const { categories: newCategories, lastDoc: newLastDoc } =
-        //     await searchCategories(searchTerm, undefined);
-        // setLastDoc(newLastDoc);
-        // setResults(newCategories);
-    }, [searchTerm]);
-
-    const [loadMore, setLoadMore] = useState(false);
-
-    const [gotAllCategories, setGotAllCategories] = useState(false);
-    useEffect(() => {
-        if (!loadMore) return;
-        if (lastDoc === undefined) {
-            setGotAllCategories(true);
-            return;
-        }
-
-        (async () => {
-            // const { categories: newCategories, lastDoc: newLastDoc } =
-            //     await searchCategories(searchTerm, lastDoc);
-            // if (lastDoc !== undefined) setLastDoc(newLastDoc);
-            // setResults((prev) => [...prev, ...newCategories]);
-            // setLoadMore(false);
-        })();
-    }, [loadMore]);
-
-    const [results, setResults] = useState<Category[]>([]);
-
-    // #endregion
-
-    const bottomRef = useRef<HTMLDivElement>(null);
-    const resultsRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        const scrollHandler = (e: Event) => {
-            if (bottomRef.current === null || resultsRef.current === null)
-                return;
-
-            const containerRect = resultsRef.current.getBoundingClientRect();
-            const bottomRect = bottomRef.current.getBoundingClientRect();
-
-            if (containerRect.bottom >= bottomRect.top - 50) setLoadMore(true);
-        };
-
-        if (bottomRef.current && resultsRef.current)
-            resultsRef.current.addEventListener("scroll", scrollHandler);
-
-        return () => {
-            if (resultsRef.current)
-                resultsRef.current.removeEventListener("scroll", scrollHandler);
-        };
-    }, [bottomRef, resultsRef, results]);
-
-    console.log(lastDoc);
-
+const Categories = () => {
     return (
         <div id="categoriesPage">
             <HomeButton />
-            <h1>
-                {purpose === CategoriesPagePurpose.VIEW_CATEGORIES
-                    ? "Browse Categories"
-                    : "Select your Categories"}
-            </h1>
-            <div className="browser">
-                <div className="searchForm">
-                    <form onSubmit={(e) => e.preventDefault()}>
-                        <div className="textInput">
-                            <input
-                                type="text"
-                                value={searchTerm}
-                                onChange={(e) => {
-                                    setResults([]);
-                                    setLastDoc(undefined);
-                                    setSearchTerm(e.target.value);
-                                    setGotAllCategories(false);
-                                    search();
-                                }}
-                            />
-                            <button onClick={search}>search</button>
-                        </div>
-                        <div className="filters"></div>
-                    </form>
-                </div>
-                <div className="resultsContainer" ref={resultsRef}>
-                    <div className="results">
-                        {results.length === 0 ? (
-                            <button className="showAll">show all</button>
-                        ) : (
-                            <>
-                                {results.map((category) => (
-                                    <Category
-                                        key={category.name}
-                                        data={category}
-                                    />
-                                ))}
-                                {!gotAllCategories ? (
-                                    <div ref={bottomRef}>
-                                        <DotSpinner />
-                                    </div>
-                                ) : (
-                                    "Thats it 😊"
-                                )}
-                            </>
-                        )}
-                    </div>
-                </div>
-            </div>
+            <h1>Browse your local categories</h1>
+            <CategoryBrowser />
         </div>
     );
 };
