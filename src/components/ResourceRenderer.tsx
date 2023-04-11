5c631563992924aab0478db4f1e7a53f11d2e4f0
NilsGke <n.j.goeke@gmail.com>
2023-04-11 22:32:03 +0200

    added resourceRender Component

---
+++ src/components/ResourceRenderer.tsx
@@ -0,0 +1,35 @@
+import { Ressource } from "../types/categoryTypes";
+import AudioPlayer from "./AudioPlayer";
+import VideoPlayer from "./VideoPlayer";
+
+const ResourceRenderer = ({
+    resource,
+    autoplay = false,
+}: {
+    resource: Ressource;
+    autoplay?: boolean;
+}) => {
+    if (resource.type === "image") {
+        const url = URL.createObjectURL(resource.content);
+        return (
+            <div className="image">
+                <img src={url} alt="" />
+            </div>
+        );
+    } else if (resource.type === "audio")
+        return (
+            <div className="audio">
+                <AudioPlayer file={resource.content} autoplay={autoplay} />
+            </div>
+        );
+    else if (resource.type === "video")
+        return <VideoPlayer file={resource.content} autoplay={autoplay} />;
+    else if (resource.type === "text")
+        return <div className="text">{resource.content}</div>;
+    // else if(resource.type === "imageCollection")
+    // return <Diashow />
+
+    return <div className="resource">unknwon content type?</div>;
+};
+
+export default ResourceRenderer;
