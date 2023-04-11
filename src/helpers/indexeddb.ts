3e129ec25a60cf4e153e43001f04b77f18e1e554
NilsGke <n.j.goeke@gmail.com>
2023-04-11 22:32:43 +0200

    added Indexed util-type, initDb and some category-storing

--- src/helpers/indexeddb.ts
+++ src/helpers/indexeddb.ts
@@ -1,41 +1,98 @@
-import { IndexedFile, MediaType, MediaTypes } from "../types/categoryTypes";
+import {
+    Category,
+    IndexedFile,
+    MediaType,
+    MediaTypes,
+} from "../types/categoryTypes";
 
-const request = window.indexedDB.open("LocalFileDatabase", 3);
-
+/** ### version documentation
+ *
+ * @version 3 all media stores
+ * @version 4 added category store
+ *
+ */
 let db: IDBDatabase;
 
-request.onerror = (event) => {
-    console.error("indexed db request threw an error", { event });
+export type Indexed<T> = T & {
+    dbIndex: number;
 };
-request.onsuccess = (event) => {
-    if (event.target === null)
-        throw new Error("indexeddb: event.target on succsess is null");
 
-    db = (event.target as IDBOpenDBRequest).result;
+export const initIndexedDB = () =>
+    new Promise<void>((resolve, reject) => {
+        const request = window.indexedDB.open("LocalFileDatabase", 4);
 
-    db.onerror = (event) => {
-        // Generic error handler for all errors targeted at this database's
-        // requests!
-        if (event.target === null)
-            throw new Error("in db.onerror: event.target is null");
+        request.onerror = (event) => {
+            console.error("indexed db request threw an error", { event });
+            reject((event.target as IDBOpenDBRequest).error);
+        };
+        // clean init
+        request.onsuccess = (event) => {
+            if (event.target === null)
+                throw new Error("indexeddb: event.target on succsess is null");
 
-        console.error(`Database error: ${(event.target as any).errorCode}`);
-    };
-};
-request.onupgradeneeded = (event) => {
-    // Save the IDBDatabase interface
-    const db = (event.target as IDBOpenDBRequest).result;
+            db = (event.target as IDBOpenDBRequest).result;
 
-    // Create an imageStore for this database
+            db.onerror = (event) => {
+                // Generic error handler for all errors targeted at this database's
+                // requests!
+                if (event.target === null)
+                    throw new Error("in db.onerror: event.target is null");
 
-    MediaTypes.forEach((type) => {
-        const objectStore = db.createObjectStore(type, {
-            autoIncrement: true,
-        });
-        objectStore.createIndex("name", "name", { unique: false });
-        objectStore.transaction.oncomplete = (event) => {};
+                console.error(
+                    `Database error: ${(event.target as any).errorCode}`
+                );
+            };
+            resolve();
+        };
+
+        // upgrade db
+        request.onupgradeneeded = async (event) => {
+            const db = (event.target as IDBOpenDBRequest).result;
+
+            console.log(
+                "%c" +
+                    `Upgrading from version ${event.oldVersion} to version ${db.version}`,
+                "color:purple;"
+            );
+
+            const proms: Promise<Event>[] = [];
+
+            // Create an stores for each media type
+            if (event.oldVersion === 0)
+                MediaTypes.forEach((type) =>
+                    proms.push(
+                        new Promise((resolve, reject) => {
+                            const objectStore = db.createObjectStore(type, {
+                                autoIncrement: true,
+                            });
+                            objectStore.createIndex("name", "name", {
+                                unique: false,
+                            });
+                            objectStore.transaction.oncomplete = resolve;
+                            objectStore.transaction.onerror = reject;
+                        })
+                    )
+                );
+
+            // create category store
+            if (event.oldVersion === 0 || event.newVersion === 4)
+                proms.push(
+                    new Promise((resolve, reject) => {
+                        const objectStore = db.createObjectStore("categories", {
+                            autoIncrement: true,
+                        });
+                        objectStore.createIndex("name", "name", {
+                            unique: false,
+                        });
+                        objectStore.transaction.oncomplete = resolve;
+                        objectStore.transaction.onerror = reject;
+                    })
+                );
+
+            await Promise.all(proms);
+            console.log("%c" + "all stores created", "color: purple;");
+        };
     });
-};
 
 export const storeFileInIndexedDB = (file: File, mediaType: MediaType) => {
     return new Promise<number>(async (resolve, reject) => {
@@ -121,4 +178,102 @@ export const deleteStoredFile = (mediaType: MediaType, index: number) =>
 
         request.onsuccess = resolve;
         request.onerror = reject;
+    });
+
+export const storeCategoryInDB = (category: Category) =>
+    new Promise<number>(async (resolve, reject) => {
+        console.log("upload category", category);
+
+        const request = db
+            .transaction("categories", "readwrite")
+            .objectStore("categories")
+            .add(category);
+
+        request.onsuccess = (e) => resolve((e.target as IDBRequest).result);
+        request.onerror = reject;
+    });
+
+export const getStoredCategory = (index: number) =>
+    new Promise<Indexed<Category>>((resolve, reject) => {
+        const request = db
+            .transaction(["categories"], "readonly")
+            .objectStore("categories")
+            .get(index);
+
+        request.onsuccess = (event) => {
+            // request still succeeds if index is not in db
+            // check for that and trigger onerror manually
+            if (request.result === undefined)
+                return request.dispatchEvent(new Event("error"));
+
+            const category: Indexed<Category> = {
+                ...(request.result as Category),
+                dbIndex: index,
+            };
+            resolve(category);
+        };
+
+        request.onerror = (event) => {
+            reject({ error: (event.target as IDBRequest).error || null });
+        };
+    });
+
+export const getStoredCategories = (startIndex: number, length: number = 5) =>
+    new Promise<Indexed<Category>[]>(async (resolve, reject) => {
+        const request = db
+            .transaction(["categories"], "readonly")
+            .objectStore("categories")
+            .openCursor(IDBKeyRange.upperBound(startIndex), "prev");
+
+        // NOTE cursor is going in reverse!
+        let i = 0;
+        const categories: Indexed<Category>[] = [];
+
+        request.onsuccess = (event) => {
+            const cursor = request.result;
+            if (cursor && i < length) {
+                const file = cursor.value as Indexed<Category>;
+                file.dbIndex = parseInt(cursor.primaryKey as string) || -1;
+                categories.push(file);
+                i++;
+                cursor.continue();
+            } else {
+                resolve(categories);
+            }
+        };
+
+        request.onerror = (event) => {
+            reject((event.target as IDBRequest).error);
+        };
+    });
+
+export const searchStoredCategories = (searchTerm: string) =>
+    new Promise<Indexed<Category>[]>((resolve, reject) => {
+        const request = db
+            .transaction(["categories"], "readonly")
+            .objectStore("categories")
+            .openCursor();
+
+        let i = 0;
+
+        const categories: Indexed<Category>[] = [];
+
+        request.onsuccess = (event) => {
+            const cursor = request.result;
+            if (cursor) {
+                const category = cursor.value as Indexed<Category>;
+                category.dbIndex = parseInt(cursor.primaryKey as string) || -1;
+                if (category.name.includes(searchTerm))
+                    categories.push(category);
+
+                i++;
+                cursor.continue();
+            } else {
+                resolve(categories);
+            }
+        };
+
+        request.onerror = (event) => {
+            reject((event.target as IDBRequest).error);
+        };
     });
