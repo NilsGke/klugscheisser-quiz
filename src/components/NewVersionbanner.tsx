f3b074432b7fe31e94d6bc282303cc740ca8d21d
NilsGke <n.j.goeke@gmail.com>
2023-04-13 16:04:43 +0200

    ~~Update~~ -> Refresh

--- src/components/NewVersionbanner.tsx
+++ src/components/NewVersionbanner.tsx
@@ -4,7 +4,7 @@ const NewVersionbanner = ({ update }: { update: () => void }) => {
     return (
         <div id="newVersionBanner">
             A new version of the app is avalible!
-            <button onClick={update}>Update</button>
+            <button onClick={update}>Refresh</button>
         </div>
     );
 };
