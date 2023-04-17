3c5f3e8ffe70d6d7c82f50259f00e60bbba00d74
NilsGke <n.j.goeke@gmail.com>
2023-04-17 21:12:59 +0200

    added boards to indexedDB

--- src/helpers/indexeddb.ts
+++ src/helpers/indexeddb.ts
@@ -1,3 +1,4 @@
+import { Board, DeletedCategory, StoredBoard } from "../types/boardTypes";
 import {
     Category,
     IndexedFile,
@@ -12,6 +13,7 @@ import {
  * @version 3 all media stores
  * @version 4 added category store
  * @version 5 added volume to audio and video
+ * @version 6 added board store
  *
  */
 let db: IDBDatabase;
@@ -20,9 +22,11 @@ export type Indexed<T> = T & {
     dbIndex: number;
 };
 
+const DB_VERSION = 6;
+
 export const initIndexedDB = () =>
     new Promise<void>((resolve, reject) => {
-        const request = window.indexedDB.open("LocalFileDatabase", 5);
+        const request = window.indexedDB.open("LocalFileDatabase", DB_VERSION);
 
         request.onerror = (event) => {
             console.error("indexed db request threw an error", { event });
@@ -140,7 +144,24 @@ export const initIndexedDB = () =>
                     })
                 );
 
-            await Promise.all(proms);
+            // create board store
+            if (event.oldVersion === 0 || event.newVersion === 6)
+                proms.push(
+                    new Promise((resolve, reject) => {
+                        const objectStore = db.createObjectStore("boards", {
+                            autoIncrement: true,
+                        });
+                        objectStore.createIndex("name", "name", {
+                            unique: false,
+                        });
+                        objectStore.transaction.oncomplete = resolve;
+                        objectStore.transaction.onerror = reject;
+                    })
+                );
+
+            await Promise.allSettled(proms).catch((error) => {
+                throw new Error("dbConversion threw error: ", error);
+            });
             console.log("%c" + "all stores created", "color: purple;");
         };
     });
@@ -231,6 +252,7 @@ export const deleteStoredFile = (mediaType: MediaType, index: number) =>
         request.onerror = reject;
     });
 
+// categories
 export const storeCategoryInDB = (category: Category) =>
     new Promise<number>(async (resolve, reject) => {
         console.log("upload category", category);
@@ -240,7 +262,11 @@ export const storeCategoryInDB = (category: Category) =>
             .objectStore("categories")
             .add(category);
 
-        request.onsuccess = (e) => resolve((e.target as IDBRequest).result);
+        request.onsuccess = (e) =>
+            resolve(
+                (e.target as IDBRequest)
+                    .result as Indexed<StoredBoard>["dbIndex"]
+            );
         request.onerror = reject;
     });
 
@@ -348,6 +374,135 @@ export const removeCategoryFromDb = (dbIndex: Indexed<Category>["dbIndex"]) =>
             .transaction("categories", "readwrite")
             .objectStore("categories")
             .delete(dbIndex);
+
+        request.onsuccess = resolve;
+        request.onerror = reject;
+    });
+
+//boards
+export const getStoredBoard = (dbIndex: Indexed<Board>["dbIndex"]) =>
+    new Promise<Indexed<Board>>((resolve, reject) => {
+        const request = db
+            .transaction(["boards"], "readonly")
+            .objectStore("boards")
+            .get(dbIndex);
+
+        request.onsuccess = async (event) => {
+            // request still succeeds if index is not in db
+            // check for that and trigger onerror manually
+            if (request.result === undefined)
+                return request.dispatchEvent(new Event("error"));
+
+            const storedBoard: Indexed<StoredBoard> = {
+                ...(request.result as StoredBoard),
+                dbIndex: dbIndex,
+            };
+            const board: Indexed<Board> = {
+                name: storedBoard.name,
+                dbIndex,
+                categories: await Promise.all(
+                    storedBoard.categoryIndexes.map(
+                        (index) =>
+                            new Promise<Indexed<Category> | DeletedCategory>(
+                                (resolve, reject) =>
+                                    getStoredCategory(index)
+                                        .then((category) => {
+                                            resolve(category);
+                                        })
+                                        .catch((error) =>
+                                            resolve({
+                                                dbIndex: index,
+                                                deleted: true,
+                                            } as DeletedCategory)
+                                        )
+                            )
+                    )
+                ),
+            };
+            resolve(board);
+        };
+
+        request.onerror = (event) => {
+            reject({ error: (event.target as IDBRequest).error || null });
+        };
+    });
+
+export const getStoredBoards = (startIndex: number, length: number) =>
+    new Promise<Indexed<Board>[]>(async (resolve, reject) => {
+        const request = db
+            .transaction(["boards"], "readonly")
+            .objectStore("boards")
+            .openCursor(IDBKeyRange.upperBound(startIndex), "prev");
+
+        // NOTE cursor is going in reverse!
+        let i = 0;
+        const promisedBoards: Promise<Indexed<Board>>[] = [];
+
+        request.onsuccess = async (event) => {
+            const cursor = request.result;
+            if (cursor && i < length) {
+                const storedBoard = cursor.value as Indexed<StoredBoard>;
+                storedBoard.dbIndex =
+                    parseInt(cursor.primaryKey as string) || -1;
+
+                const board = getStoredBoard(storedBoard.dbIndex);
+
+                promisedBoards.push(board);
+
+                i++;
+                cursor.continue();
+            } else {
+                const boards = await Promise.all(promisedBoards);
+                resolve(boards);
+            }
+        };
+
+        request.onerror = (event) => {
+            reject((event.target as IDBRequest).error);
+        };
+    });
+
+export const storeBoardInDB = (board: Board) =>
+    new Promise<Indexed<StoredBoard>["dbIndex"]>((resolve, reject) => {
+        console.log("store board", board);
+
+        const boardToStore: StoredBoard = {
+            name: board.name,
+            categoryIndexes: board.categories.map((c) => c.dbIndex),
+        };
+
+        const request = db
+            .transaction("boards", "readwrite")
+            .objectStore("boards")
+            .add(boardToStore);
+
+        request.onsuccess = (e) => resolve((e.target as IDBRequest).result);
+        request.onerror = reject;
+    });
+
+export const updateBoardInDB = (board: Indexed<Board>) =>
+    new Promise<Indexed<StoredBoard>["dbIndex"]>((resolve, reject) => {
+        console.log("update category", board);
+
+        const boardToStore: StoredBoard = {
+            name: board.name,
+            categoryIndexes: board.categories.map((c) => c.dbIndex),
+        };
+
+        const request = db
+            .transaction(["boards"], "readwrite")
+            .objectStore("boards")
+            .put(boardToStore, board.dbIndex);
+
+        request.onsuccess = (e) => resolve((e.target as IDBRequest).result);
+        request.onerror = reject;
+    });
+export const removeBoardFromDB = (index: Indexed<Board>["dbIndex"]) =>
+    new Promise((resolve, reject) => {
+        const request = db
+            .transaction("boards", "readwrite")
+            .objectStore("boards")
+            .delete(index);
 
         request.onsuccess = resolve;
         request.onerror = reject;
