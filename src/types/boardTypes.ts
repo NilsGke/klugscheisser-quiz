d4557e43661a91b6c9947f4bc84e5ba9913ea78c
NilsGke <n.j.goeke@gmail.com>
2023-04-17 21:24:29 +0200

    bord types

---
+++ src/types/boardTypes.ts
@@ -0,0 +1,20 @@
+import { Indexed } from "../helpers/indexeddb";
+import { Category } from "./categoryTypes";
+
+export type DeletedCategory = {
+    dbIndex: number;
+    deleted: true;
+};
+
+export const categoryIsDeleted = (
+    category: DeletedCategory | Category
+): category is DeletedCategory => category.hasOwnProperty("deleted");
+
+export interface Board {
+    name: string;
+    categories: (Indexed<Category> | DeletedCategory)[];
+}
+
+export interface StoredBoard extends Omit<Board, "categories"> {
+    categoryIndexes: Indexed<Category>["dbIndex"][];
+}
