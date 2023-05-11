ecc72a3efb7b00011abc20f55438054d78bcba86
NilsGke <n.j.goeke@gmail.com>
2023-05-11 22:25:06 +0200

    renamed file and function

--- src/helpers/base64ToFile.ts
+++ src/helpers/base64ToFile.ts
@@ -3,7 +3,7 @@
  * note this should be removed and not used in production
  * @source [stackoverflow](https://stackoverflow.com/questions/35940290/how-to-convert-base64-string-to-javascript-file-object-like-as-from-file-input-f#:~:text=Way%201%3A%20only%20works%20for%20dataURL%2C%20not%20for%20other%20types%20of%20url.)
  */
-export function dataURLtoFile(dataurl: string, filename: string) {
+export function base64ToFile(dataurl: string, filename: string) {
     var arr = dataurl.split(","),
         mime = (arr[0].match(/:(.*?);/) as string[])[1],
         bstr = atob(arr[1]),
