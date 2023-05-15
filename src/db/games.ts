423e434118428901f0b48054c8c0faca2ac70d3d
NilsGke <n.j.goeke@gmail.com>
2023-05-15 21:21:10 +0200

    adapt to use thing store

--- src/db/games.ts
+++ src/db/games.ts
@@ -1,40 +1,8 @@
 import { Game } from "$types/gameTypes";
-import { db } from "./indexeddb";
+import { getThing, removeThing, setThing } from "./things";
 
-export const saveGameInDb = (game: Game) =>
-    new Promise<void>((resolve, reject) => {
-        const request = db
-            .transaction("games", "readwrite")
-            .objectStore("games")
-            .put(game, 0);
+export const saveGameInDb = (game: Game) => setThing("activeGame", game);
 
-        request.onsuccess = () => resolve();
-        request.onerror = () => reject();
-    });
+export const getGameFromDb = () => getThing<Game>("activeGame");
 
-export const getGameFromDb = () =>
-    new Promise<Game>((resolve, reject) => {
-        const request = db
-            .transaction("games", "readonly")
-            .objectStore("games")
-            .get(0);
-
-        request.onsuccess = () => {
-            if (request.result === undefined)
-                return request.dispatchEvent(new Event("error"));
-
-            resolve(request.result as Game);
-        };
-        request.onerror = () => reject();
-    });
-
-export const deleteGameFromDb = () =>
-    new Promise<void>((resolve, reject) => {
-        const request = db
-            .transaction("games", "readwrite")
-            .objectStore("games")
-            .delete(0);
-
-        request.onsuccess = () => resolve();
-        request.onerror = () => reject();
-    });
+export const deleteGameFromDb = () => removeThing("activeGame");
