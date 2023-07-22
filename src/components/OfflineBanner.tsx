b0fead9c7fcf5719aa0e382a5884219c2e3761a6
NilsGke <n.j.goeke@gmail.com>
2023-07-22 19:55:20 +0200

    option to hide offline banner

--- src/components/OfflineBanner.tsx
+++ src/components/OfflineBanner.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import "./OfflineBanner.scss";
 
 const OfflineBanner = () => {
-    return (
-        <div id="offlineBanner">
-            you are currently offline, some things might not be avalible
-        </div>
-    );
+    const [hidden, setHidden] = useState(false);
+    if (!hidden)
+        return (
+            <div id="offlineBanner">
+                you are currently offline, some things might not be avalible
+                <button onClick={() => setHidden(true)}>ok</button>
+            </div>
+        );
+
+    return null;
 };
 
 export default OfflineBanner;
