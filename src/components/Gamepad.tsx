273eb0d943688d2dc1647ace75e7ba5c9ac93a45
NilsGke <n.j.goeke@gmail.com>
2023-07-22 15:47:43 +0200

    use gamepad id (- >multiple gamepads with same buttons) and multiple buttons per team

--- src/components/Gamepad.tsx
+++ src/components/Gamepad.tsx
@@ -2,10 +2,13 @@ import { useState, useEffect, useCallback } from "react";
 import "./Gamepad.scss";
 import gamepadIcon from "$assets/gamepad.svg";
 import closeIcon from "$assets/close.svg";
+import addIcon from "$assets/add.svg";
 import useGamepad from "$hooks/useGamepad";
 import { GameTeam } from "$types/gameTypes";
 import { getThing, setThing } from "$db/things";
 import useKeyboard from "$hooks/keyboard";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import React from "react";
 
 const Gamepad = ({
     teams,
@@ -16,18 +19,43 @@ const Gamepad = ({
 }) => {
     const [overlayVisible, setOverlayVisible] = useState(false);
 
-    const [mapping, setMapping] = useState<number[]>([0, 5, 10, 15]);
+    const [mapping, setMapping] = useState<string[][]>([[], [], [], [], []]);
 
     // get mapping
     useEffect(() => {
         getThing<typeof mapping>("gamepadMapping")
-            .then(setMapping)
-            .catch(() => setThing("gamepadMapping", mapping));
+            .then((mapping) => {
+                // check for old mapping type
+                if (typeof mapping[0] === "number")
+                    throw Error("old mapping type");
+
+                console.log({ mapping, m: mapping.length, t: teams.length });
+
+                if (mapping.length < teams.length)
+                    mapping.push(
+                        ...new Array(teams.length - mapping.length).fill([])
+                    );
+
+                setMapping(mapping);
+                setThing("gamepadMapping", mapping);
+            })
+            .catch(() =>
+                setThing("gamepadMapping", new Array(teams.length).fill([]))
+            );
     }, []);
 
     // gamepad stuff
-    const buttonpress = (id: number) => {
-        const teamIndex = mapping.findIndex((keyId) => keyId === id);
+    const buttonpress = ({
+        button,
+        gamepad,
+    }: {
+        button: number;
+        gamepad: number;
+    }) => {
+        const teamIndex = mapping.findIndex(
+            (keyIdArray) =>
+                !!keyIdArray.find((keyId) => keyId === gamepad + "." + button)
+        );
 
         if (teamIndex >= 0) {
             buzzer(teams[teamIndex], teamIndex);
@@ -76,62 +104,142 @@ const Mapping = ({
     update,
 }: {
     teams: GameTeam[];
-    mapping: number[];
-    update: (newMapping: number[]) => void;
+    mapping: string[][];
+    update: (newMapping: typeof mapping) => void;
 }) => {
-    // edit mapping
-    const [editing, setEditing] = useState<false | number>(false);
+    const [addingButton, setAddingButton] = useState<false | { team: number }>(
+        false
+    );
 
-    useKeyboard((key) => key === "Escape" && setEditing(false));
+    const { connected } = useGamepad((pressed) => {
+        if (!addingButton) return;
+        const newMapping = mapping.slice();
+        const teamsMapping = newMapping[addingButton.team];
+        teamsMapping[newMapping[addingButton.team].length] =
+            pressed.gamepad + "." + pressed.button;
+        const unique = [...new Set(teamsMapping)];
+        newMapping[addingButton.team] = unique;
+        setThing("gamepadMapping", newMapping).then(() => {
+            update(newMapping);
+        });
+    });
+
+    useKeyboard((key) => key === "Escape" && setAddingButton(false));
+
+    const [mappingTableRef] = useAutoAnimate();
 
     return (
-        <div className="mapping">
-            <h2>Controller support</h2>
-            {teams.map((team, i) => {
-                const keyId = mapping[i];
-                return (
-                    <div className="team" key={team.name + i}>
-                        <div className="id">#{i + 1}</div>
-                        <div
-                            className="name"
-                            style={{
-                                color: team.color,
-                            }}
-                        >
-                            {team.name}
-                        </div>
-                        {editing === i ? (
-                            <EditButton
-                                select={(buttonId) => {
-                                    const newMapping = mapping.slice();
-                                    newMapping[i] = buttonId;
-                                    setThing("gamepadMapping", newMapping).then(
-                                        () => {
+        <>
+            <div className="mapping">
+                <h2>Controller support</h2>
+                <div className="mappingTable" ref={mappingTableRef}>
+                    {teams.map((team, teamIndex) => {
+                        const keyIdArray = mapping[teamIndex] || [];
+                        return (
+                            <React.Fragment key={teamIndex}>
+                                <div className="id">#{teamIndex + 1}</div>
+                                <div
+                                    className="name"
+                                    style={{
+                                        color: team.color,
+                                    }}
+                                >
+                                    {team.name}
+                                </div>
+                                <ButtonList
+                                    keyIdArray={keyIdArray}
+                                    adding={
+                                        addingButton !== false &&
+                                        addingButton.team === teamIndex
+                                    }
+                                    startAdding={() =>
+                                        setAddingButton({
+                                            team: teamIndex,
+                                        })
+                                    }
+                                    stopAdding={() => setAddingButton(false)}
+                                    remove={(keyIndex: number) => {
+                                        const newMapping = mapping.slice();
+                                        console.log(newMapping);
+
+                                        const newKeyIdArr = keyIdArray.slice();
+                                        newKeyIdArr.splice(keyIndex, 1);
+
+                                        newMapping[teamIndex] = newKeyIdArr;
+
+                                        setThing(
+                                            "gamepadMapping",
+                                            newMapping
+                                        ).then(() => {
                                             update(newMapping);
-                                            setEditing(false);
-                                        }
-                                    );
-                                }}
-                            />
-                        ) : (
-                            <button
-                                className={
-                                    "button" + (editing === i ? " editing" : "")
-                                }
-                                onClick={() => setEditing(i)}
-                            >
-                                {keyId === undefined ? "not assigned" : keyId}
-                            </button>
-                        )}
-                    </div>
-                );
-            })}
+                                        });
+                                    }}
+                                />
+                            </React.Fragment>
+                        );
+                    })}
+                </div>
+            </div>
+            {!connected ? "no gamepad connected" : null}
+        </>
+    );
+};
+
+const ButtonList = ({
+    keyIdArray,
+    remove,
+    adding,
+    startAdding,
+    stopAdding,
+}: {
+    keyIdArray: string[];
+    remove: (index: number) => void;
+    adding: boolean;
+    stopAdding: () => void;
+    startAdding: () => void;
+}) => {
+    const [buttonListRef] = useAutoAnimate();
+
+    return (
+        <div className="buttons" ref={buttonListRef}>
+            {keyIdArray.map((keyId, keyIndex) => (
+                <Button
+                    keyId={keyId}
+                    onClick={() => remove(keyIndex)}
+                    key={keyId}
+                />
+            ))}
+            {adding ? (
+                <button
+                    key={"add"}
+                    className="stop"
+                    onClick={stopAdding}
+                    title="stop adding buttons to this team"
+                >
+                    ✅
+                </button>
+            ) : (
+                <button
+                    key={"add"}
+                    className="add"
+                    onClick={startAdding}
+                    title="add buttons to this team"
+                >
+                    +
+                </button>
+            )}
         </div>
     );
 };
 
-const EditButton = ({ select }: { select: (buttonId: number) => void }) => {
-    const { connected } = useGamepad((id) => select(id));
-    if (connected) return <button className="active">press key</button>;
-    else return <button disabled>no controller connected</button>;
+const Button = ({ keyId, onClick }: { keyId: string; onClick: () => void }) => {
+    return (
+        <button
+            className={"button"}
+            title="remove this button"
+            onClick={onClick}
+        >
+            {keyId === undefined ? "not assigned" : keyId}
+        </button>
+    );
 };
